321b23898d509738c45e09f7b8d644b8e2198202
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-08-14 17:26:34 +0500

    outfit font

--- mocktor-ai/app/page.tsx
+++ mocktor-ai/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="h1">
-      
+      <Button>Subscribe to SayEDU Channel</Button>
   </div>
   );
 }
